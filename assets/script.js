a37d995eae1027ea88d53bc38ec153749f973747
dvespa08 <danielle.vespa@gmail.com>
2022-05-20 10:39:51 -0400

    password generator functions

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,84 @@
 // Assignment code here
 
-var passwordLength = [];
-console.log(passwordLength.length);
-for(var i=0; i <passwordLength.length; i++) {
-  console.log(passwordLength[i]);
-}
+var passwordBase = [];
 
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-console.log(lowerCase.length);
-for(var i=0; i <lowerCase.length; i++) {
-  console.log(lowerCase[i]);
-}
 
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-console.log(upperCase.length);
-for(var i=0; i <upperCase.length; i++) {
-  console.log(upperCase[i]);
-}
 
-var passwordNumeric = ['1','2','3','4','5','6','7','8','9'];
-console.log(passwordNumeric.length);
-for(var i=0; i <passwordNumeric.length; i++) {
-  console.log(passwordNumeric[i]);
-}
+var numbers = ['1','2','3','4','5','6','7','8','9'];
 
-var passwordCharacters = ['!','$','%','&','*',];
-console.log(passwordCharacters.length);
-for(var i=0; i <passwordCharacters.length; i++) {
-  console.log(passwordCharacters[i]);
-}
+var special = ['!','$','%','&','*',];
 
+//series of questions for what the user wants to include in password
 function generatePassword () {
   var passwordLength = window.prompt("How many characters do you want your password to be? (Enter a number between 8 - 128)");
   console.log(passwordLength);
+  if (passwordLength <8 || passwordLength >128) {
+    window.alert ("Please select a number between 8 and 128")
+    generatePassword()
+  } else {
+  promptLowercase()
+  }
 
-  var lowerCase = window.prompt ("Do you want to lower case text? (Enter yes or no)");
-  console.log(lowerCase);
+//uses the confirm alert to select okay or cancel which equals true or false
+  function promptLowercase() {
+    var userWantsLowercase = confirm ("Press okay to include lower case or cancel to not include")
+    console.log(userWantsLowercase);
+    if (userWantsLowercase === true){
+        passwordBase = passwordBase.concat(lowerCase)
+        console.log(passwordBase)
+        promptUppercase()
+    } else {
+        promptUppercase()
+    }
+  }
 
-  var upperCase = window.prompt ("Do you want to include upper case text? (Enter yes or no)");
-  console.log(upperCase);
+  function promptUppercase() {
+    var userWantsUppercase = confirm ("Press okay to include upper case or cancel to not include.")
+    console.log(userWantsUppercase);
+    if (userWantsUppercase === true) {
+      passwordBase = passwordBase.concat(upperCase)
+      console.log(passwordBase);
+      promptNumbers ()
+    } else {
+      promptNumbers ()
+    }
+  }
 
-  var passwordNumeric = window.prompt ("Do you want to include numbers? (Enter yes or no)");
-  console.log(passwordNumeric);
+  function promptNumbers () {
+    var userWantsNumbers = confirm ("Press okay to include numbers or cancel to not include.")
+    console.log(userWantsNumbers);
+    if (userWantsNumbers === true) {
+      passwordBase = passwordBase.concat(numbers)    
+      console.log(passwordBase);
+      promptSpecial ()  
+    } else {
+      promptSpecial ()
+    }
+  }
 
-  var passwordCharacters =window.prompt ("Do you want to include special characters? (Enter yes or no)");
-  console.log(passwordCharacters);
+  function promptSpecial () {
+    var userWantsSpecial = confirm ("Press okay to include special characters or cancel to not include.")
+    console.log(userWantsSpecial);
+    if (userWantsSpecial === true) {
+      passwordBase = passwordBase.concat(special)
+      console.log(passwordBase);
+    }
+  }
 
+  console.log(passwordBase)
+  console.log(passwordLength)
+  passwordBase = passwordBase.join("")
+  console.log(passwordBase)
+  var finalPassword = ""
+  for (var i =0; i< passwordLength; i++){
+    finalPassword += passwordBase.charAt(Math.floor(Math.random() *passwordBase.length))
+    console.log(finalPassword);
+  }
+  return finalPassword;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
